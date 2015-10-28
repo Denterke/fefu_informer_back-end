7791a196937bf5f2430aa2598da899401fc6b6cb
Denter <ADGFQ121519@gmail.com>
2015-10-28 18:10:03 +0000

    Working Post Register User

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
 var Hapi = require('hapi'); //require с hapi
-var pg = require('pg'); //require с postgres
+//var db = require('./dbConnection');
 
 // Create a server with a host and port
 var server = new Hapi.Server();
-var client = new pg.Client("postgres://postgres:bafffefu123@31.131.24.188:5432/fefu_informer_db");
-
 server.connection({
     host: '31.131.24.188',
     port: 8080
 });
 
-client.connect();
 
-// TestConnection to DB
 server.route({
     method: 'GET',
     path:'/',
     handler: function (request, reply) {
-      client.query('SELECT first_name FROM users', function(err, result) {
-        if(err) throw err;
-
-        reply (result.rows[0].first_name);
-      });
+      reply('Server WORK!');
     }
 });
 
-// Test Post method
+// register Users
 require('./registerUsers')(server);
 
 // Start the server
--- registerUsers.js
+++ registerUsers.js
@@ -1,13 +1,44 @@
+var fs = require('fs'); //file system
+var db = require('./dbConnection');
+
 var registerUser = function(server) {
+  db.connect();
   server.route({
     method: 'POST',
     path:'/registerUsers',
     handler: function (request, reply) {
-      var yourReq = request.payload;
-      console.log(request.payload);
-      reply('Your request: ' + yourReq);
+      var firstName = decodeURI(request.payload.firstName);
+      var lastName  = decodeURI(request.payload.lastName);
+      var avatar    = request.payload.fileUpload;
+
+      if (avatar)
+        fs.writeFile(
+          'images/users_avatars/'+firstName+'avatar.jpg',
+          avatar,
+          function(err) {
+            if (err) throw err;
+          }
+        );
+
+      db.query("INSERT INTO users (first_name, last_name) VALUES ('" +firstName+ "','" +lastName+"')", function(err, result) {
+        if(err) throw err;
+        reply('new user added!');
+      });
+
+      db.end();
     }
   });
+
+  server.register(require('inert'), function () {}); //for working with static file
+  server.route({
+    method: 'GET',
+    path:'/registerUsers',
+      handler: function (request, reply) {
+        reply('ok');
+        //reply.file('images/users_avatars/'+firstName+'avatar.jpg');
+      }
+  });
+
 };
 
 module.exports = registerUser;
