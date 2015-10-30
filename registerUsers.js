99790f6c115b05091bc375d4e25136cf9dfb5bdf
Denter <ADGFQ121519@gmail.com>
2015-10-30 16:18:45 +0000

    Working Register v.2 and Validation

--- registerUsers.js
+++ registerUsers.js
@@ -1,6 +1,8 @@
 var fs = require('fs'); //file system
-var pg = require('pg'); //require с postgres
+var Joi = require('joi');
+var pg = require('pg'); //require postgres
 var conString = "postgres://postgres:bafffefu123@31.131.24.188:5432/fefu_informer_db";
+var randomString = require('random-string');
 
 var registerUser = function(server) {
   server.route({
@@ -12,39 +14,59 @@ var registerUser = function(server) {
       var lastName    = decodeURI(request.payload.lastName);
       var avatar      = request.payload.fileUpload;
       var phoneNumber = request.payload.phoneNumber;
-      var avatarSrc   = 'images/users_avatars/'+lastName+'avatar.jpg'; //сделать уникальное имя
+      var avatarSrc   = 'images/users_avatars/default_avatar.png'; //сделать уникальное имя
       var response    = 'new user added!';
 
-      if (avatar)
-        fs.writeFile(
-          avatarSrc,
-          avatar,
-          function(err) {
-            if (err) throw err;
-          }
-        );
+      var schema = {
+        firstName: Joi.string().regex(/[\u0400-\u04FF]/).min(3).max(15).required(),
+        lastName: Joi.string().regex(/[\u0400-\u04FF]/).min(5).max(20).required(),
+        phoneNumber: Joi.string().regex(/^\d+$/).length(11)
+      };
+      var value = {
+        firstName: firstName,
+        lastName: lastName,
+        phoneNumber: phoneNumber
+      };
 
-      pg.connect(conString, function(err, client, done) {
+      Joi.validate(value, schema, function (err, value) {
         if (err) {
-          response = 'new user not added!';
-          return console.error('could not connect to postgres', err);
+          response = err.details[0].message;
+          console.log(err.details[0].message);
         }
+        else {
+          if (avatar) {//дописать проверку расширения
+            avatarSrc   = 'images/users_avatars/'+randomString()+lastName+'.jpg'; //сделать уникальное имя
+            fs.writeFile(
+              avatarSrc,
+              avatar,
+              function(err) {
+                if (err) throw err;
+              }
+            );
+          }
 
-        client.query(
-          "INSERT INTO users (first_name, last_name, phone_number, avatar_src) VALUES ('" +firstName+ "','"+lastName+"','"+phoneNumber+"','"+avatarSrc+"')",
-          function(err, result) {
-            done();
+          pg.connect(conString, function(err, client, done) {
             if (err) {
               response = 'new user not added!';
-              throw err;
+              return console.error('could not connect to postgres', err);
             }
 
-          console.log(firstName, lastName, phoneNumber);
-        });
-      });
+            client.query(
+              "INSERT INTO users (first_name, last_name, phone_number, avatar_src) VALUES ('" +firstName+ "','"+lastName+"','"+phoneNumber+"','"+avatarSrc+"')",
+              function(err, result) {
+                done();
+                if (err) {
+                  response = 'new user not added!';
+                  throw err;
+                }
+
+              console.log(firstName, lastName, phoneNumber);
+            });
+          });
+        }
+    });
 
     reply(response);
-
     }
   });
 };
