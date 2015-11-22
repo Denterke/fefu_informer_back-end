4f172316cb9d406c90927e80f969f36f4ad11a65
Denter <ADGFQ121519@gmail.com>
2015-11-22 16:41:25 +0000

    Driving, ORM, Refactoring and many other

---
+++ events/additionEvents.js
@@ -0,0 +1,81 @@
+var fs           = require('fs'); //file system
+var joi          = require('joi'); // module of validation
+var dbTables     = require('../databaseModels/mappingModel'); // map of database tables
+var randomString = require('random-string'); // generate a random string
+
+var additionEvents = function (server) {
+  server.route({
+    method: 'POST',
+    path:'/additionEvents',
+    handler: function (request, reply) {
+      var users         = dbTables.users; // select table users
+      var events        = dbTables.events; // select table events
+
+      var title         = decodeURI(request.payload.title);       // were problems with cirilic
+      var description   = decodeURI(request.payload.description);
+      var image         = request.payload.fileUpload;
+      var eventsStatus  = request.payload.eventsStatus;
+      var imgSrc        = 'images/eventsList/default_events.jpg';
+      var phoneNumber   = request.payload.phoneNumber.substring(1);
+
+      var response      = ''; // response from server to client
+
+      //validating entered data
+      var schema = {
+        _title          : joi.string().min(5).max(50).required(),
+        _description    : joi.string().min(10).max(160).required(),
+        _eventsStatus   : joi.string().regex(/^(true|false)$/).required(),
+        _phoneNumber    : joi.string().regex(/^\d+$/).length(10)
+      };
+
+      var value = {
+        _title          : title,
+        _description    : description,
+        _eventsStatus   : eventsStatus,
+        _phoneNumber    : phoneNumber
+      };
+
+      joi.validate(value, schema, function (err, value) {
+        if (err)
+          return reply(require('../auxiliaries/validatorsHandler')(err.details[0].message)).code(400); // validation's errors hadler
+
+        if (image) {
+          imgSrc   = 'images/eventsline/'+randomString()+title+'.jpg';
+
+          //if image is in base64
+          if ((typeof(image) == 'string') && (image.indexOf('base64') != -1))
+            image = new Buffer(image.replace(/^data:image\/(png|jpg|jpeg);base64,/, ""), "base64");
+
+          fs.writeFile(
+            imgSrc,
+            image,
+            function(err) {
+              if (err) throw err;
+          });
+        };
+
+        //adding events, only if the user is certified!
+        users.find({ phone_number: phoneNumber, confirmation: true }).only("first_name", "last_name").run(function (err, user) {
+          if (user.length === 0) {
+            response = JSON.stringify({ "status": 400, "message": "Пользователь не верефицирован, добавление невозможно!" });
+            return reply(response).code(400);
+          };
+
+          // if user is certified
+          events.create({
+            title: title,
+            description: description,
+            img_src: imgSrc,
+            is_official: eventsStatus,
+            creator: user[0].first_name + " " + user[0].last_name },
+            function (err) {
+              response = JSON.stringify({ "status": 200, "message": "Новость успешно добавлена!" });
+              return reply(response).code(200);
+          });
+        });
+      });
+    }
+  });
+};
+
+module.exports = additionEvents;
---
+++ events/eventsList.js
@@ -0,0 +1,45 @@
+var dbTables = require('../databaseModels/mappingModel'); // map of database tables
+
+var eventsList = function (server) {
+  server.route({
+    method: 'GET',
+    path:'/eventsList/{offsetValue}&{eventsStatus}',
+    handler: function (request, reply) {
+      var events     = dbTables.events; // select table events
+
+      var response = ''; // response from server to client
+
+      var eventsStatus;
+
+      switch (request.params.eventsStatus) {
+        case "all":
+          eventsStatus = [true, false];
+          break;
+        case "off":
+          eventsStatus = true;
+          break;
+        case "unoff":
+          eventsStatus = false;
+          break;
+        default:
+          response = JSON.stringify({ "status": 400, "message": "Не найдено событий с такими параметрами!" });
+          return reply(response).code(400);
+
+      };
+
+      // selection of events, consider the type of events, the response size - 5 events
+      events.find({ is_official: eventsStatus }).limit(5).offset(request.params.offsetValue).run(function (err, events) {
+        if (events.length === 0) {
+          response = JSON.stringify({ "status": 200, "message": "События закончились!" });
+          return reply(response).code(200);
+        }
+        else {
+          response = events;
+          return reply(response).code(200);
+        };
+      });
+    }
+  });
+};
+
+module.exports = eventsList;
